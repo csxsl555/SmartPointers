e28f9191890de78fbd18aa9f1b117a70c51d6407
csxsl555 <csxsi@sjtu.edu.cn>
2025-10-15 21:38:59 +0800

    Finish Shared_ptr.hpp and Weak_ptr.hpp.

--- include/Shared_ptr.hpp
+++ include/Shared_ptr.hpp
@@ -1,56 +1,75 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
 
+template <typename T> class WeakPtr;
+
 template <typename T>
 class SharedPtr {
   private:
     // Add whatever you want to add.
     T* ptr;          // Raw pointer to the managed object
     int* ref_count; // Pointer to the reference count
+    int* weak_count; // Pointer to the weak reference count
+    friend class WeakPtr<T>; // Declare WeakPtr as a friend class
+
   public:
     // Constructors
-    SharedPtr() : ptr(nullptr), ref_count(nullptr) {};         // Default constructor
+    SharedPtr() : ptr(nullptr), ref_count(nullptr), weak_count(nullptr) {};         // Default constructor
+    SharedPtr(const WeakPtr<T>& temp_wp); // Constructor from WeakPtr
     explicit SharedPtr(T* p) : ptr(p) {
         if (p) {
             ref_count = new int(1);
+            weak_count = new int(0);
         } else {
             ref_count = nullptr;
+            weak_count = nullptr;
         }
     };            // Constructor from raw pointer
-    SharedPtr(const SharedPtr& tmp) : ptr(tmp.ptr), ref_count(tmp.ref_count) {
+    SharedPtr(const SharedPtr& tmp) : ptr(tmp.ptr), ref_count(tmp.ref_count), weak_count(tmp.weak_count) {
         if (ptr) {
             ++(*ref_count);
         }
     };     // Copy constructor
-    SharedPtr(SharedPtr&& tmp) noexcept : ptr(tmp.ptr), ref_count(tmp.ref_count) {
+    SharedPtr(SharedPtr&& tmp) noexcept : ptr(tmp.ptr), ref_count(tmp.ref_count), weak_count(tmp.weak_count) {
         tmp.ptr = nullptr;
         tmp.ref_count = nullptr;
+        tmp.weak_count = nullptr;
     }; // Move constructor
 
     // Destructor
     ~SharedPtr() {
-      if (ref_count) {
-          --(*ref_count);    
-          if (*ref_count == 0) {
-              delete ptr;
-              delete ref_count;
-          }
+      if (ref_count && weak_count) {
+        --(*ref_count);    
+        if (*ref_count == 0) {
+          delete ptr;
+          ptr = nullptr;
+        }
+        if (*ref_count == 0 && *weak_count == 0) {
+          delete weak_count;
+          delete ref_count;
+        }
       }
-      ptr = nullptr;
-      ref_count = nullptr;
     }; 
 
     // Assignment operators
     SharedPtr& operator=(const SharedPtr& temp) {
         if (this != &temp) {
             // Decrement the old reference count
-            if (ptr && --(*ref_count) == 0) {
+            if (ref_count && weak_count) {
+              --(*ref_count);    
+              if (*ref_count == 0) {
                 delete ptr;
+                ptr = nullptr;
+              }
+              if (*ref_count == 0 && *weak_count == 0) {
+                delete weak_count;
                 delete ref_count;
+              }
             }
             // Copy the data from temp
             ptr = temp.ptr;
             ref_count = temp.ref_count;
+            weak_count = temp.weak_count;
             if (ptr) {
                 ++(*ref_count);
             }
@@ -60,15 +79,24 @@ class SharedPtr {
     SharedPtr& operator=(SharedPtr&& temp) noexcept {
         if (this != &temp) {
             // Decrement the old reference count
-            if (ptr && --(*ref_count) == 0) {
-                delete ptr;
-                delete ref_count;
+            if (ref_count && weak_count) {
+                --(*ref_count);    
+                if (*ref_count == 0) {
+                    delete ptr;
+                    ptr = nullptr;
+                }
+                if (*ref_count == 0 && *weak_count == 0) {
+                    delete weak_count;
+                    delete ref_count;
+                }
             }
             // Move the data from temp
             ptr = temp.ptr;
             ref_count = temp.ref_count;
+            weak_count = temp.weak_count;
             temp.ptr = nullptr;
             temp.ref_count = nullptr;
+            temp.weak_count = nullptr;
         }
         return *this;
     }; // Move assignment operator
@@ -77,6 +105,12 @@ class SharedPtr {
     int use_count() const {
         return ref_count ? *ref_count : 0;
     }; // Get the number of shared owners
+    int* use_count_ptr() const {
+        return ref_count;
+    }; // Get the pointer to the reference count
+    int* weak_count_ptr() const {
+        return weak_count;
+    }; // Get the pointer to the reference count
     T* get() const {
         return ptr;
     };        // Get the raw pointer
@@ -90,23 +124,40 @@ class SharedPtr {
         return ptr;
     }; // Arrow operator
     void reset() {
-        if (ptr && --(*ref_count) == 0) {
+        if (ref_count && weak_count) {
+          --(*ref_count);    
+          if (*ref_count == 0) {
             delete ptr;
+            ptr = nullptr;
+          }
+          if (*ref_count == 0 && *weak_count == 0) {
+            delete weak_count;
             delete ref_count;
+          }
         }
         ptr = nullptr;
         ref_count = nullptr;
+        weak_count = nullptr;
     };          // Reset to empty state
     void reset(T* p) {
-        if (ptr && --(*ref_count) == 0) {
+        if (ref_count && weak_count) {
+          --(*ref_count);    
+          if (*ref_count == 0) {
             delete ptr;
+            ptr = nullptr;
+          }
+          if (*ref_count == 0 && *weak_count == 0) {
+            delete weak_count;
             delete ref_count;
+          }
         }
         ptr = p;
         if (p) {
             ref_count = new int(1);
+            weak_count = new int(0);
         } else {
             ref_count = nullptr;
+            weak_count = nullptr;
         }
     };      // Reset with a new raw pointer
 };
--- include/Weak_ptr.hpp
+++ include/Weak_ptr.hpp
@@ -1,36 +1,145 @@
+#include <utility>
 #ifndef WEAK_PTR
 #define WEAK_PTR
 
 #include "Shared_ptr.hpp" // Include SharedPtr definition
-// When submitting to ACMOJ, please replace this with the content inside Shared_ptr.hpp
+// When submitting to damn ACMOJ, please replace this with the content inside Shared_ptr.hpp
 
 template <typename T>
 class WeakPtr {
   private:
     // Add whatever you want to add.
+    T* ptr;        // Pointer to the managed object
+    int* ref_count; // Pointer to the SHARED reference count
+    int* weak_count; // Pointer to the WEAK reference count
+    friend class SharedPtr<T>; // Declare SharedPtr as a friend class
   public:
     // Constructors
-    WeakPtr();                         // Default constructor
-    WeakPtr(const SharedPtr<T>& temp); // Constructor from SharedPtr
-    WeakPtr(const WeakPtr& temp);      // Copy constructor
-    WeakPtr(WeakPtr&& temp);           // Move constructor
+    WeakPtr() : ptr(nullptr), ref_count(nullptr), weak_count(nullptr) {};                         // Default constructor
+    WeakPtr(const SharedPtr<T>& temp) : ptr(temp.get()), ref_count(temp.use_count_ptr()), weak_count(temp.weak_count_ptr()) {
+        if (weak_count) {
+            ++(*weak_count);
+        }
+    }; // Constructor from SharedPtr
+    WeakPtr(const WeakPtr& temp) : ptr(temp.ptr), ref_count(temp.ref_count), weak_count(temp.weak_count) {
+      if (weak_count) {
+          ++(*weak_count);
+      }
+    };      // Copy constructor
+    WeakPtr(WeakPtr&& temp) noexcept : ptr(temp.ptr), ref_count(temp.ref_count), weak_count(temp.weak_count) {
+      temp.ptr = nullptr;
+      temp.ref_count = nullptr;
+      temp.weak_count = nullptr;
+    };           // Move constructor
 
     // Destructor
-    ~WeakPtr();
+    ~WeakPtr() {
+      if (ref_count && weak_count) {
+          --(*weak_count);
+          if (*weak_count == 0 && *ref_count == 0) {
+              delete weak_count;
+              delete ref_count;
+          }
+      }
+      ptr = nullptr;
+      ref_count = nullptr;
+      weak_count = nullptr;
+    };
 
     // Assignment operators
-    WeakPtr& operator=(const WeakPtr& temp);      // Copy assignment operator
-    WeakPtr& operator=(const SharedPtr<T>& temp); // Assignment from SharedPtr
-    WeakPtr& operator=(WeakPtr&& temp);           // Move assignment operator
+    
+    WeakPtr& operator=(const WeakPtr& temp) {
+      if (this != &temp) {
+        if (ref_count && weak_count) {
+          --(*weak_count);
+          if (*weak_count == 0 && *ref_count == 0) {
+              delete weak_count;
+              delete ref_count;
+          }
+        }
+        // Copy the data from temp
+        ptr = temp.ptr;
+        ref_count = temp.ref_count;
+        weak_count = temp.weak_count;
+        if (weak_count) {
+            ++(*weak_count);
+        }
+      }
+      return *this;
+    };      // Copy assignment operator
+    WeakPtr& operator=(const SharedPtr<T>& temp) {
+      if (ref_count && weak_count) {
+          --(*weak_count);
+          if (*weak_count == 0 && *ref_count == 0) {
+              delete weak_count;
+              delete ref_count;
+          }
+      }
+      // Copy the data from temp
+      ptr = temp.get();
+      ref_count = temp.use_count_ptr();
+      weak_count = temp.weak_count_ptr();
+      if (weak_count) {
+          ++(*weak_count);
+      }
+      return *this;
+    }; // Assignment from SharedPtr
+    WeakPtr& operator=(WeakPtr&& temp) {
+      if (this != &temp) {
+        if (ref_count && weak_count) {
+            --(*weak_count);
+            if (*weak_count == 0 && *ref_count == 0) {
+                delete weak_count;
+                delete ref_count;
+            }
+        }
+        // Move the data from temp
+        ptr = temp.ptr;
+        ref_count = temp.ref_count;
+        weak_count = temp.weak_count;
+        temp.ptr = nullptr;
+        temp.ref_count = nullptr;
+        temp.weak_count = nullptr;
+      }
+      return *this;
+    };           // Move assignment operator
 
     // Basic operations
-    int use_count();     // Get the number of shared owners
-    SharedPtr<T> lock(); // Get a SharedPtr to the managed object
-    bool expired();      // Check if the managed object was deleted
-    void reset();        // Reset to empty state
+    int use_count() const {
+      return ref_count ? *ref_count : 0;
+    };     // Get the number of shared owners
+    bool expired() const {
+      return use_count() == 0;
+    };      // Check if the managed object was deleted
+    operator bool() const {
+        return !expired();
+    }; // Check if the pointer is not null
+    SharedPtr<T> lock() const {
+      if (expired()) {
+        return SharedPtr<T>();
+      } else {
+        return SharedPtr<T>(*this);
+      }
+    }; // Get a SharedPtr to the managed object
+    void reset() {
+      if (weak_count) {
+          --(*weak_count);
+          if (*weak_count == 0 && *ref_count == 0) {
+              delete weak_count;
+              delete ref_count;
+          }
+      }
+      ptr = nullptr;
+      ref_count = nullptr;
+      weak_count = nullptr;
+    };        // Reset to empty state
 
     // Utility functions
-    void swap(WeakPtr& other) noexcept; // Swap with another WeakPtr
+    void swap(WeakPtr& other) noexcept {
+      std::swap(ptr, other.ptr);
+      std::swap(ref_count, other.ref_count);
+      std::swap(weak_count, other.weak_count);
+    }; // Swap with another WeakPtr
 };
 
 // Non-member swap function
@@ -38,5 +147,12 @@ template <typename T>
 void swap(WeakPtr<T>& lhs, WeakPtr<T>& rhs) noexcept {
     lhs.swap(rhs);
 }
+
+template <typename T>
+SharedPtr<T>::SharedPtr(const WeakPtr<T>& temp_wp) : ptr(temp_wp.ptr), ref_count(temp_wp.ref_count), weak_count(temp_wp.weak_count) {
+    if (ptr && ref_count) {
+        ++(*ref_count);
+    }
+}; // Constructor from WeakPtr
 
 #endif // WEAK_PTR
